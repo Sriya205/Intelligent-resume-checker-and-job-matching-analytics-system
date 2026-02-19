5a39151c68b21bd0ec7ca4d22a30f073ff485398
Shalu <shalubhardwaj410@gmail.com>
2026-02-19 11:28:08 +0530

    Added the code for resume.py and update the main.py

--- Backend/app/main.py
+++ Backend/app/main.py
@@ -1,12 +1,23 @@
 from fastapi import FastAPI
-from .database import engine
-from .models import Base
-from . import jobs, resumes, ranking
+from app.config import engine, Base
 
-app = FastAPI()
+# Import ALL models (VERY IMPORTANT)
+from app.models import candidate, job, resume, email_log
 
+# Import routers
+from app.routes import jobs, resumes, ranking
+
+app = FastAPI(title="Intelligent Resume Checker API")
+
+# Create database tables
 Base.metadata.create_all(bind=engine)
 
+# Include routers
 app.include_router(jobs.router)
 app.include_router(resumes.router)
 app.include_router(ranking.router)
+
+
+@app.get("/")
+def root():
+    return {"message": "Backend Running Successfully 🚀"}
