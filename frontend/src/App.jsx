6ec33c91f632c4824964ea8a832c008a987226ca
Shalu <shalubhardwaj410@gmail.com>
2026-03-01 13:37:12 +0530

    frontend initiated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,54 +1,35 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { AuthProvider } from "./context/AuthContext";
-import ProtectedRoute from "./components/layout/ProtectedRoute";
-import Layout from "./components/layout/Layout";
-
-import Login from "./pages/Login";
-import Dashboard from "./pages/Dashboard";
-import ResumeScreening from "./pages/ResumeScreening";
-import CandidateRanking from "./pages/CandidateRanking";
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
+  const [count, setCount] = useState(0)
+
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Login />} />
-
-        <Route
-          path="/dashboard"
-          element={
-            <ProtectedRoute>
-              <Layout>
-                <Dashboard />
-              </Layout>
-            </ProtectedRoute>
-          }
-        />
-
-        <Route
-          path="/resume"
-          element={
-            <ProtectedRoute>
-              <Layout>
-                <ResumeScreening />
-              </Layout>
-            </ProtectedRoute>
-          }
-        />
-
-        <Route
-          path="/ranking"
-          element={
-            <ProtectedRoute>
-              <Layout>
-                <CandidateRanking />
-              </Layout>
-            </ProtectedRoute>
-          }
-        />
-      </Routes>
-    </BrowserRouter>
-  );
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
 }
 
-export default App;
+export default App
